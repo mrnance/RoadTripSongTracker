714e211e6833d5bb2f35d0f2f269ed6bc6213c23
marknance <marknance@hotmail.com>
2023-09-12 21:40:54 -0700

    Add main.py file which contains TODO lists

---
+++ main.py
@@ -0,0 +1,23 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+### TODO Call to Spotify to get recent history
+# TODO Call secrets file to get log-in data and Spotify account
+# TODO Error handling of API call
+# TODO Give option of synthetic data (Taylor Switf discography; include RR)
+
+### TODO Convert incoming data to objects. Preserve timestamps
+# TODO JSON parser 
+
+### TODO Search for replicate songs
+# TODO Decide if better to search the JSON or better to search the objects.
+
+### TODO Store data (SQL database)
+# TODO Each user gets their own table?
+
+### TODO Webpage asking which songs to add to playlist
+# TODO Checkboxes for adding to playlist
+# TODO Determine if playlist should be 
+# TODO Webpage with link to final playlist
+
+print("End of program")
